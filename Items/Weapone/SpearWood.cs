a68f89d707e930b4dbea53a747c9eaac71dc8730
Leandro Boxler <leandroboxler@gmail.com>
2025-08-20 12:50:10 -0300

    Se agrego la primera pieza de equipo "lanza de madera"

--- Items/Weapone/SpearWood.cs
+++ Items/Weapone/SpearWood.cs
@@ -6,6 +6,7 @@ public class SpearWood : IWeapone
     public SpearWood() : base("Lanza de madera") { }
     public override void Use(IHero hero)
     {
+        hero.Weapone="Lanza de madera";
         hero.Attack = 40;
 
     }
